a90a0b70b219d1acbf16dc14291cd2aca5e9d1ee
nayangoelsjsu <nayan.goel@sjsu.edu>
2017-05-11 22:48:56 -0700

    All Classes
    All Classes

---
+++ Petrolpump_202/Petrolpump_202/DisplayScreen.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class DisplayScreen here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class DisplayScreen extends Actor
+{
+    /**
+     * Act - do whatever the DisplayScreen wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
